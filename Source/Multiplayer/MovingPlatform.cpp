9f4a27e599d123b5e19bc9594c4c45da9f319b98
k03302 <unisonman660@gmail.com>
2025-02-19 01:50:20 +0900

    Init project

---
+++ Source/Multiplayer/MovingPlatform.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    Position1 = CreateDefaultSubobject<USceneComponent>(TEXT("Position1"));
+    Position2 = CreateDefaultSubobject<USceneComponent>(TEXT("Position2"));
+    PlatformHolder = CreateDefaultSubobject<USceneComponent>(TEXT("Platform"));
+
+    SetRootComponent(Root);
+    Position1->SetupAttachment(Root);
+    Position2->SetupAttachment(Root);
+    PlatformHolder->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Alpha = InitAlpha;
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    Alpha = FMath::Clamp(Alpha + DeltaTime * Speed, 0.0f, 1.0f);
+    if (Alpha == 1 || Alpha == 0)
+    {
+        Speed = -Speed;
+    }
+
+    FVector NewPlatformLocation = Position1->GetComponentLocation() * Alpha + Position2->GetComponentLocation() * (1 - Alpha);
+    PlatformHolder->SetWorldLocation(NewPlatformLocation);
+}
