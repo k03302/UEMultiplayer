356e67bba30b61ccf044cfeeb7a3eaa8f3175469
k03302 <unisonman660@gmail.com>
2025-02-25 13:49:22 +0900

    Add trigger to PlatformTrigger

--- Source/Multiplayer/PlatformTrigger.cpp
+++ Source/Multiplayer/PlatformTrigger.cpp
@@ -1,27 +1,53 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlatformTrigger.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 APlatformTrigger::APlatformTrigger()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
+    TriggerVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerVolume"));
+    if (!ensure(TriggerVolume != nullptr))
+        return;
+    SetRootComponent(TriggerVolume);
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+    TriggerVolume->OnComponentBeginOverlap.AddDynamic(
+        this, &APlatformTrigger::OnComponentBeginOverlap);
+    TriggerVolume->OnComponentEndOverlap.AddDynamic(
+        this, &APlatformTrigger::OnComponentEndOverlap);
+    UE_LOG(LogTemp, Warning, TEXT("start"));
 }
 
 // Called every frame
 void APlatformTrigger::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
+void APlatformTrigger::OnComponentBeginOverlap(
+    UPrimitiveComponent *OverlappedComponent,
+    AActor *OtherActor,
+    UPrimitiveComponent *OtherComp,
+    int32 OtherBodyIndex,
+    bool bFromSweep,
+    const FHitResult &SweepResult)
+{
+    UE_LOG(LogTemp, Warning, TEXT("Activated: %s"), *OtherComp->GetName());
+}
+
+void APlatformTrigger::OnComponentEndOverlap(
+    UPrimitiveComponent *OverlappedComponent,
+    AActor *OtherActor,
+    UPrimitiveComponent *OtherComp,
+    int32 OtherBodyIndex)
+{
+    UE_LOG(LogTemp, Warning, TEXT("Dectivated: %s"), *OtherComp->GetName());
+}
--- Source/Multiplayer/PlatformTrigger.h
+++ Source/Multiplayer/PlatformTrigger.h
@@ -9,18 +9,37 @@
 UCLASS()
 class MULTIPLAYER_API APlatformTrigger : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	APlatformTrigger();
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    APlatformTrigger();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
+private:
+    UPROPERTY(VisibleAnywhere)
+    class UBoxComponent *TriggerVolume;
+
+    UFUNCTION()
+    void OnComponentBeginOverlap(
+        UPrimitiveComponent *OverlappedComponent,
+        AActor *OtherActor,
+        UPrimitiveComponent *OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult &SweepResult);
+
+    UFUNCTION()
+    void OnComponentEndOverlap(
+        UPrimitiveComponent *OverlappedComponent,
+        AActor *OtherActor,
+        UPrimitiveComponent *OtherComp,
+        int32 OtherBodyIndex);
 };
